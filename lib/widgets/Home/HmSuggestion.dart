055422810e1c59fdc6ef1609148e4147457d57a9
jinxuan <894133325@qq.com>
2026-06-05 16:23:57 +0800

    实现安卓端适配

--- lib/widgets/Home/HmSuggestion.dart
+++ lib/widgets/Home/HmSuggestion.dart
@@ -68,38 +68,40 @@ class _HmSuggestionState extends State<HmSuggestion> {
     List<GoodsItem> list = _getDisplayItem(); // 取到三条数据
     return List.generate(list.length, (int index) {
       // 对每个数据图片构建并设置样式
-      return Column(
-        // 这里设置Column的原因是上图片下文字
-        children: [
-          // 新组件ClipRRect，包裹子组件，可以裁剪图片，设置圆角
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8), // 和container的格式一样
-            child: Image.network(
-              // errorBuilder用于当网络图片出现404时，用一个新部件替换原有部件
-              errorBuilder: (context, error, stackTrace) {
-                return Image.asset(
-                  "lib/assets/home_cmd_inner.png",
-                  width: 100,
-                  height: 140,
-                  fit: BoxFit.cover,
-                ); // 返回本地图片保证不会出现显示失败
-              },
-              list[index].picture,
-              fit: BoxFit.cover,
-              width: 100,
-              height: 140,
+      return Expanded(
+        child: Column(
+          // 这里设置Column的原因是上图片下文字
+          children: [
+            // 新组件ClipRRect，包裹子组件，可以裁剪图片，设置圆角
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8), // 和container的格式一样
+              child: Image.network(
+                // errorBuilder用于当网络图片出现404时，用一个新部件替换原有部件
+                errorBuilder: (context, error, stackTrace) {
+                  return Image.asset(
+                    "lib/assets/home_cmd_inner.png",
+                    // width: 100,
+                    height: 140,
+                    fit: BoxFit.cover,
+                  ); // 返回本地图片保证不会出现显示失败
+                },
+                list[index].picture,
+                fit: BoxFit.cover,
+                // width: 100,
+                height: 140,
+              ),
             ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 237, 100, 90),
-              borderRadius: BorderRadius.circular(12),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 237, 100, 90),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Text("￥${list[index].price}", style: TextStyle(fontSize: 13)),
             ),
-            child: Text("￥${list[index].price}"),
-          ),
-        ],
+          ],
+        ),
       );
     });
   }
@@ -124,12 +126,14 @@ class _HmSuggestionState extends State<HmSuggestion> {
           children: [
             // 顶部内容
             _buildHeader(),
-            SizedBox(height: 10),
+            // SizedBox(width: 10),
             Row(
               children: [
                 _buildLeft(),
+                SizedBox(width: 5),
                 Expanded(
                   child: Row(
+                    spacing: 5,
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 主轴对齐均分
                     children: _getChildrenList(),
                   ),
